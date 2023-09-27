213b95be9f7c97e97e053cf9f18d11d9718ac3d9
Joseph Kageche <ngumi98@gmail.com>
2023-09-27 20:56:10 +0300

    ready player 1

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a function that returns a list of lists of integer rep the Pascals's triangle of n
+"""
+def pascal_triangle(n):
+    """func to rep the pascals's triangle
+    n (int)
+    returns an empty list if n <= 0
+    """
+
