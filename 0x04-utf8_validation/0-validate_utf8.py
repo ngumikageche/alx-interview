5e3c493124a817a21a7e1c925ae454243f429f07
Joseph Kageche <ngumi98@gmail.com>
2023-10-26 19:13:32 +0300

    0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+def validUTF8(data):
+    """Initialize a variable to track the number
+      of bytes for the current character"""
+    bytes_to_follow = 0
+
+    for byte in data:
+        """ Check the two most significant bits of the byte"""
+        byte_as_bin = format(byte, '08b')
+        if bytes_to_follow == 0:
+            if byte_as_bin[0] == '0':
+                continue
+            """Count the number of consecutive 1s starting from the left"""
+            while byte_as_bin.startswith('10'):
+                byte_as_bin = byte_as_bin[2:]
+                bytes_to_follow -= 1
+            """ Determine the number of bytes to follow """
+            if byte_as_bin.startswith('110'):
+                bytes_to_follow = 1
+            elif byte_as_bin.startswith('1110'):
+                bytes_to_follow = 2
+            elif byte_as_bin.startswith('11110'):
+                bytes_to_follow = 3
+            else:
+                return False
+        else:
+            if not byte_as_bin.startswith('10'):
+                return False
+            bytes_to_follow -= 1
+    return bytes_to_follow == 0
