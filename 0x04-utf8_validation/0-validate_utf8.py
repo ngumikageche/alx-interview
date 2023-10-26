f04492314c79f5d2a05e7760d2853308ef7df955
Joseph Kageche <ngumi98@gmail.com>
2023-10-26 19:17:36 +0300

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""a method that determines if a given data set represents
+ a valid UTF-8 encoding"""
+
+
 def validUTF8(data):
     """Initialize a variable to track the number
       of bytes for the current character"""
